bf7e661aedd6ef278af0c39a9f8e9568b12cb128
laurenbenichou <laurenbenichou@gmail.com>
2014-07-30 14:27:45 -0700

    added hack for touch devices

--- js/main.js
+++ js/main.js
@@ -61,11 +61,11 @@ function drag(){
         $("#score").removeClass("green")
       }
       if(candy_ing=="m"){
-        $(".answer").html("<span class='right'>RIGHT!</span>Many types of black licorice are made with molasses, which may be a source of lead contamination.")
+        $(".answer").html("<span class='right'>RIGHT! </span>Many types of black licorice are made with molasses, which may be a source of lead contamination.")
       }else if(candy_ing=="g"){
-        $(".answer").html("<span class='right'>RIGHT!</span>Ginger candies often contain lead.")
+        $(".answer").html("<span class='right'>RIGHT! </span>Ginger candies often contain lead.")
       }else if(candy_ing =="p"){
-        $(".answer").html("<span class='right'>RIGHT!</span>Did you know that candy, toys and jewelry can all be sources of lead exposure?")
+        $(".answer").html("<span class='right'>RIGHT! </span>Did you know that candy, toys and jewelry can all be sources of lead exposure?")
       }else if(candy_ing=="panda"){
         $(".answer").html("<span class='right'>RIGHT! </span>In July 2014, Panda was one of several companies that signed a legal agreement to reformulate their products so they would no exceed the legal limit for lead.")
       }else if(candy_ing == "tj"){
@@ -75,11 +75,11 @@ function drag(){
       }else if(candy_ing == "tj4"){
         $(".answer").html("<span class='right'>RIGHT! </span>In July 2014, Trader Joe's was one of several companies that signed a legal agreement to reformulate their products so they would no longer exceed the legal limit for lead.")
       }else if(candy_ing == "santos"){
-        $(".answer").html("<span class='right'>RIGHT! </span> Did you know that according to California law, businesses are required to warn customers when the amount of lead in food exceeds ten parts per billion?")
+        $(".answer").html("<span class='right'>RIGHT! </span>Did you know that according to California law, businesses are required to warn customers when the amount of lead in food exceeds ten parts per billion?")
       }else if(candy_ing == "rm"){
-        $(".answer").html("<span class='right'>RIGHT! </span> Unlike most types of black licorice, red licorice does not contain molasses, which is thought to be a likely source of lead contamination.")
+        $(".answer").html("<span class='right'>RIGHT! </span>Unlike most types of black licorice, red licorice does not contain molasses, which is thought to be a likely source of lead contamination.")
       }else{
-        $(".answer").html("<span class='right'>RIGHT!</span> Nice job!")
+        $(".answer").html("<span class='right'>RIGHT! </span>Nice job!")
       }
     }else if (lead_status != candy_status ){
       ui.draggable.draggable('option','revert',true)
@@ -92,15 +92,15 @@ function drag(){
         $("#score").removeClass("green")
       }
       if(candy_ing=="m"){
-        $(".answer").html("<span class='wrong'>WRONG!</span> Remember, some candy with molasses often contains lead.")
+        $(".answer").html("<span class='wrong'>WRONG! </span>Remember, some candy with molasses often contains lead.")
       }else if(candy_ing=="g"){
-        $(".answer").html("<span class='wrong'>WRONG!</span> Ginger candies often contain lead.")
+        $(".answer").html("<span class='wrong'>WRONG! </span>Ginger candies often contain lead.")
       }else if(candy_ing=="panda"){
-        $(".answer").html("<span class='wrong'> WRONG! </span> Even though 'all natural' candies sound better for you, they may also be more likely to contain lead when an ingredient (molasses) has become contaminated.")
+        $(".answer").html("<span class='wrong'> WRONG! </span>Even though 'all natural' candies sound better for you, they may also be more likely to contain lead when an ingredient (molasses) has become contaminated.")
       }else if(candy_ing=="tj3"){
         $(".answer").html("<span class='wrong'> WRONG! </span>Although Red Vines&reg; black licorice was recalled for lead contamination in August 2012, the FDA cleared the product a few months later.")
       }else{
-        $(".answer").html("<span class='wrong'>WRONG!</span> Try again!")
+        $(".answer").html("<span class='wrong'>WRONG! </span>Try again!")
       }
     }
   }
